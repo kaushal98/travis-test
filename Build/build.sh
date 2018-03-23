ee60df5fd84cd52fe623437e8592b7aef543ea6e
Kaushal Patel <kaushalpatel@gmail.com>
2018-03-22 21:39:15 -0400

    update

--- Build/build.sh
+++ Build/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get install {gcc,g++}-arm-linux-gnueabi{,hf}
+sudo apt-get install {gcc,g++}-arm-linux-gnueabi{,hf}
 arm-linux-gnueabhihf-g++ main.cpp -o hello
 git clone https://github.com/openssl/openssl
 git checkout OpenSSL_1_0_2-stable
