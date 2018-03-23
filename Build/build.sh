c4b61f40b7581d070ae7d589f0005221b3b33fab
Kaushal Patel <kaushalpatel@gmail.com>
2018-03-22 23:21:35 -0400

    link openssl

--- Build/build.sh
+++ Build/build.sh
@@ -24,6 +24,6 @@ echo "*******************************************"
 g++ main.cpp -o hello_x86_64 -lssl -lcrypto
 file hello_x86_64
 ./hello_x86_64
-${CROSS}-g++ -Wall -L/usr/${CROSS}/lib -I/usr/${CROSS}/include/openssl/ main.cpp -lssl -lcrypto
+${CROSS}-g++ -Wall -L/usr/${CROSS}/lib -I/usr/${CROSS}/include/openssl/ main.cpp -o hello_arm -lssl -lcrypto
 file hello_arm
 
