1134617cfaabf47758b6299472bf1a70254c857f
Kaushal Patel <kaushalpatel@gmail.com>
2018-03-22 21:30:05 -0400

    try armv7l cross-compile

--- Build/build.sh
+++ Build/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
-g++ main.cpp -o hello
+sudo apt-get install {gcc,g++}-arm-linux-gnueabi{,hf}
+arm-linux-gnueabhihf-g++ main.cpp -o hello
+git clone https://github.com/openssl/openssl
+git checkout OpenSSL_1_0_2
