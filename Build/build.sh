2f12947657b0e4e84cbf329ab65b2bdfb1098f65
Kaushal Patel <kaushalpatel@gmail.com>
2018-08-08 23:21:42 -0400

    update build.sh

--- Build/build.sh
+++ Build/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-g++ main.cpp -o hello_x86_64
+g++ main.cpp -fprofile-arcs -ftest-coverage -o hello_x86_64
 file hello_x86_64
 ./hello_x86_64
 #${CROSS}-g++ -Wall -L/usr/${CROSS}/lib -I/usr/${CROSS}/include/openssl/ main.cpp -o hello_arm -lssl -lcrypto
